1347a46383ff8484fdecda03e7129e8a6220b8be
SafaaMahbub <safaamahbub@Safaas-MacBook-Pro.local>
2026-04-01 14:02:28 -0400

    reformated the sort method

--- src/main/java/Preprocessing.java
+++ src/main/java/Preprocessing.java
@@ -4,37 +4,36 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.*;
 import java.util.stream.Collectors;
-import org.apache.spark.ml.feature.MinMaxScaler;
-import org.apache.spark.ml.feature.MinMaxScalerModel;
+import java.util.Map.*;
+
 
 public class Preprocessing {
 
-    String file = "borg_traces_data.csv";
+    String file = "test_csv.csv";
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Preprocessing p = new Preprocessing();
         List<Row> test = p.loadValues(p.file);
-        System.out.println("There are" + test.size()+ "rows");
-        Map<String, List<Row>> sorted_list = p.sortByTaskAndTime(test);
-        //System.out.println(sorted_list.toString());
-        Map<Row, Vector<Integer>> oneHotEncoding = p.oneHotEncoding(test);
-        System.out.println(oneHotEncoding.toString());
+        System.out.println("There are" + test.size() + "rows");
+        Queue<Entry<Long, List<Row>>> sorted_list = p.sortByTaskAndTime(test);
+        System.out.println(sorted_list.toString());
+        /*Map<Row, Vector<Integer>> oneHotEncoding = p.oneHotEncoding(test);
+        System.out.println(oneHotEncoding.toString())*/;
 
 
     }
 
-    public List<Row> loadValues(String path)
-    {
+    public List<Row> loadValues(String path) {
         List<Row> lines = new ArrayList<>();
-        try{
+        try {
             BufferedReader bf = new BufferedReader(new FileReader(path));
             String row;
             bf.readLine(); //skipping the first line
             bf.readLine(); //skip the second line
-            while((row = bf.readLine())!=null){
+            while ((row = bf.readLine()) != null) {
                 String[] data = parseLine(row);
-                if(data.length<10 || Arrays.stream(data).anyMatch(s -> s == null || s.trim().isEmpty())){
-                   continue;
+                if (data.length < 10 || Arrays.stream(data).anyMatch(s -> s == null || s.trim().isEmpty())) {
+                    continue;
                 }
                 try {
                     long timestamp = Long.parseLong(data[1]);
@@ -54,10 +53,9 @@ public class Preprocessing {
                     double assignedMemory = Double.parseDouble(data[10]);
 
 
-                    Row dataRow = new Row(timestamp, eventType, collectionID, startTime,endTime,avgUsageCPU,avgUsageMemory,maxUsageCPU,maxUsageMemory,assignedMemory);
+                    Row dataRow = new Row(timestamp, eventType, collectionID, startTime, endTime, avgUsageCPU, avgUsageMemory, maxUsageCPU, maxUsageMemory, assignedMemory);
                     lines.add(dataRow);
-                }
-                catch (NumberFormatException e){
+                } catch (NumberFormatException e) {
                     continue;
                 }
             }
@@ -66,57 +64,52 @@ public class Preprocessing {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-    return lines;
+        return lines;
     }
 
-    public static String[] parseLine(String line){
+    public static String[] parseLine(String line) {
         return line.split(",");
     }
 
-    public Map<String, List<Row>> sortByTaskAndTime(List<Row> data){
-        Map<String, List<Row>> reformatData = data.stream()
-                .collect(Collectors.groupingBy(Row::getCollectionID));
-
-        for(List<Row> currentRow : reformatData.values())
-        {
-            currentRow.sort(Comparator.comparingDouble(Row::getTimestamp));
-        }
-        return reformatData;
+    public Queue<Entry<Long, List<Row>>> sortByTaskAndTime(List<Row> data) {
+        return data.stream().collect(
+                        Collectors.groupingBy(
+                                r -> Long.parseLong(r.getCollectionID())
+                        )
+                )
+                .entrySet()
+                .stream()
+                .map(e -> Map.entry(e.getKey(), e.getValue().stream().sorted(Comparator.comparingLong(Row::getTimestamp)).toList()))
+                .collect(Collectors.toCollection(
+                                () -> new PriorityQueue<>(Entry.<Long, List<Row>>comparingByKey().reversed())
+                        )
+                );
     }
 
-    public Map<Row, Vector<Integer>> oneHotEncoding (List<Row> data)
-    {
-        int [] uniqueEventTypes = {0,1,2,3,4,5,6,7,8,9,10};
+    public Map<Row, Vector<Integer>> oneHotEncoding(List<Row> data) {
+        int[] uniqueEventTypes = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
         /*Map<Integer,Integer> eventTypeToVectorIndex = new HashMap<>();
         int i = 0;
         for(int type: uniqueEventTypes){
             eventTypeToVectorIndex.put(type,i++);
         }*/
-        Map <Row, Vector<Integer>> encodingMap = new HashMap<>();
-        for(Row r : data)
-        {
+        Map<Row, Vector<Integer>> encodingMap = new HashMap<>();
+        for (Row r : data) {
             int index = r.getEventType();
             Vector<Integer> v = new Vector<>();
             v.setSize(11);
-            for(int j = 0; j<v.size();j++){
-                if (j == index){
+            for (int j = 0; j < v.size(); j++) {
+                if (j == index) {
                     v.set(j, 1);
-                }
-                else{
+                } else {
                     v.set(j, 0);
                 }
-                encodingMap.put(r,v);
+                encodingMap.put(r, v);
             }
         }
         return encodingMap;
     }
-
-
-
-
-
-
 
 
 }
